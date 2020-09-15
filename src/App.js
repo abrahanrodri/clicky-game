be9318406ee7a6e30238984fa1b3c90901ee6ccc
Abe <abrahan.rodri@gmail.com>
2020-09-14 21:56:00 -0400

    trying again

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">
-            CARTOON NETWORK CLICKY GAME
+            CARTOON 90s CLICKY GAME
           </h1>
         </header>
         <h3 className="App-intro">
